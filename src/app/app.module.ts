83163c7353b26cd5fcec440e120814384d7a466d
Ledangviet <73343108+Ledangviet@users.noreply.github.com>
2023-06-11 17:22:55 +0700

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	modified:   src/main.ts 	modified:   tsconfig.app.json 	modified:   tsconfig.spec.json 	src/app/config/ 	src/app/detaildialog/ 	src/app/home/ 	src/app/model/ 	src/assets/config.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { DetaildialogComponent } from './detaildialog/detaildialog.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetaildialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    GridModule,
+    BrowserAnimationsModule,
+    NavigationModule,
+    DialogsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
